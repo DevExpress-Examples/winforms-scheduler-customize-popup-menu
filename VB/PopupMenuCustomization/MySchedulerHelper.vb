a7b4ea0806f473933f2a36cbc1f10902decaebfd
DevExpressExampleBot <fakeEmail>
2023-09-05 13:25:16 +0400

    Source auto update [skip ci]

--- VB/PopupMenuCustomization/MySchedulerHelper.vb
+++ VB/PopupMenuCustomization/MySchedulerHelper.vb
@@ -1,11 +1,12 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Text
+Imports System
 Imports DevExpress.XtraScheduler
 
 Namespace PopupMenuCustomization
+
     Friend Class MySchedulerHelper
-        Public Shared Users() As String = { "Peter Dolan", "Ryan Fischer", "Andrew Miller", "Tom Hamlett", "Jerry Campbell", "Carl Lucas", "Mark Hamilton", "Steve Lee" }
+
+        Public Shared Users As String() = New String() {"Peter Dolan", "Ryan Fischer", "Andrew Miller", "Tom Hamlett", "Jerry Campbell", "Carl Lucas", "Mark Hamilton", "Steve Lee"}
+
         Public Shared Sub FillResources(ByVal storage As SchedulerStorage, ByVal count As Integer)
             Dim resources As ResourceCollection = storage.Resources.Items
             storage.BeginUpdate()
@@ -15,11 +16,10 @@ Namespace PopupMenuCustomization
                     Dim resource As Resource = storage.CreateResource(i)
                     resource.Caption = Users(i - 1)
                     resources.Add(resource)
-                Next i
+                Next
             Finally
                 storage.EndUpdate()
             End Try
         End Sub
-
     End Class
 End Namespace
