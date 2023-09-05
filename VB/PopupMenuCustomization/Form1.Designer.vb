8362cb941c9c63b86b71e88d2de58e9905142066
DevExpressExampleBot <fakeEmail>
2023-09-05 13:24:13 +0400

    Source auto update [skip ci]

--- VB/PopupMenuCustomization/Form1.Designer.vb
+++ VB/PopupMenuCustomization/Form1.Designer.vb
@@ -1,5 +1,7 @@
-﻿Namespace PopupMenuCustomization
-    Partial Public Class Form1
+Namespace PopupMenuCustomization
+
+    Partial Class Form1
+
         ''' <summary>
         ''' Required designer variable.
         ''' </summary>
@@ -10,28 +12,28 @@
         ''' </summary>
         ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Windows Form Designer generated code"
-
+'#Region "Windows Form Designer generated code"
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
         ''' </summary>
         Private Sub InitializeComponent()
             Me.components = New System.ComponentModel.Container()
-            Dim timeRuler1 As New DevExpress.XtraScheduler.TimeRuler()
-            Dim timeRuler2 As New DevExpress.XtraScheduler.TimeRuler()
+            Dim timeRuler1 As DevExpress.XtraScheduler.TimeRuler = New DevExpress.XtraScheduler.TimeRuler()
+            Dim timeRuler2 As DevExpress.XtraScheduler.TimeRuler = New DevExpress.XtraScheduler.TimeRuler()
             Me.schedulerControl1 = New DevExpress.XtraScheduler.SchedulerControl()
             Me.schedulerStorage1 = New DevExpress.XtraScheduler.SchedulerStorage(Me.components)
             Me.panel1 = New System.Windows.Forms.Panel()
             Me.label1 = New System.Windows.Forms.Label()
-            DirectCast(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(Me.schedulerStorage1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.schedulerControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.schedulerStorage1), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.panel1.SuspendLayout()
             Me.SuspendLayout()
             ' 
@@ -41,12 +43,13 @@
             Me.schedulerControl1.Location = New System.Drawing.Point(0, 32)
             Me.schedulerControl1.Name = "schedulerControl1"
             Me.schedulerControl1.Size = New System.Drawing.Size(701, 325)
-            Me.schedulerControl1.Start = New Date(2010, 10, 8, 0, 0, 0, 0)
+            Me.schedulerControl1.Start = New System.DateTime(2010, 10, 8, 0, 0, 0, 0)
             Me.schedulerControl1.Storage = Me.schedulerStorage1
             Me.schedulerControl1.TabIndex = 0
             Me.schedulerControl1.Text = "schedulerControl1"
             Me.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler1)
             Me.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler2)
+            AddHandler Me.schedulerControl1.PopupMenuShowing, New DevExpress.XtraScheduler.PopupMenuShowingEventHandler(AddressOf Me.schedulerControl1_PopupMenuShowing)
             ' 
             ' panel1
             ' 
@@ -76,20 +79,20 @@
             Me.Controls.Add(Me.panel1)
             Me.Name = "Form1"
             Me.Text = "Form1"
-            DirectCast(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(Me.schedulerStorage1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.schedulerControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.schedulerStorage1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.panel1.ResumeLayout(False)
             Me.panel1.PerformLayout()
             Me.ResumeLayout(False)
-
         End Sub
 
-        #End Region
+'#End Region
+        Private schedulerControl1 As DevExpress.XtraScheduler.SchedulerControl
 
-        Private WithEvents schedulerControl1 As DevExpress.XtraScheduler.SchedulerControl
         Private schedulerStorage1 As DevExpress.XtraScheduler.SchedulerStorage
+
         Private panel1 As System.Windows.Forms.Panel
+
         Private label1 As System.Windows.Forms.Label
     End Class
 End Namespace
-
--- VB/PopupMenuCustomization/Form1.vb
+++ VB/PopupMenuCustomization/Form1.vb
@@ -1,21 +1,18 @@
-﻿Imports System
-Imports System.Collections.Generic
+Imports System
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
-#Region "#usings"
+'#Region "#usings"
 Imports DevExpress.Utils.Menu
 Imports DevExpress.XtraScheduler
 Imports DevExpress.XtraScheduler.Services
 Imports DevExpress.XtraScheduler.Commands
+
 ' ...
-#End Region ' #usings
-
-
+'#End Region  ' #usings
 Namespace PopupMenuCustomization
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
 
         Public Sub New()
@@ -24,41 +21,34 @@ Namespace PopupMenuCustomization
             schedulerControl1.Views.DayView.DayCount = 2
         End Sub
 
-#Region "#popupmenushowing"
-Private Sub schedulerControl1_PopupMenuShowing(ByVal sender As Object, ByVal e As PopupMenuShowingEventArgs) Handles schedulerControl1.PopupMenuShowing
-    If e.Menu.Id = DevExpress.XtraScheduler.SchedulerMenuItemId.DefaultMenu Then
-        ' Hide the "Change View To" menu item.
-        Dim itemChangeViewTo As SchedulerPopupMenu = e.Menu.GetPopupMenuById(SchedulerMenuItemId.SwitchViewMenu)
-        itemChangeViewTo.Visible = False
+'#Region "#popupmenushowing"
+        Private Sub schedulerControl1_PopupMenuShowing(ByVal sender As Object, ByVal e As PopupMenuShowingEventArgs)
+            If e.Menu.Id = SchedulerMenuItemId.DefaultMenu Then
+                ' Hide the "Change View To" menu item.
+                Dim itemChangeViewTo As SchedulerPopupMenu = e.Menu.GetPopupMenuById(SchedulerMenuItemId.SwitchViewMenu)
+                itemChangeViewTo.Visible = False
+                ' Remove unnecessary items.
+                e.Menu.RemoveMenuItem(SchedulerMenuItemId.NewAllDayEvent)
+                ' Disable the "New Recurring Appointment" menu item.
+                e.Menu.DisableMenuItem(SchedulerMenuItemId.NewRecurringAppointment)
+                ' Find the "New Appointment" menu item and rename it.
+                Dim item As SchedulerMenuItem = e.Menu.GetMenuItemById(SchedulerMenuItemId.NewAppointment)
+                If item IsNot Nothing Then item.Caption = "&New Meeting"
+                ' Create a menu item for the Scheduler command.
+                Dim service As ISchedulerCommandFactoryService = CType(schedulerControl1.GetService(GetType(ISchedulerCommandFactoryService)), ISchedulerCommandFactoryService)
+                Dim cmd As SchedulerCommand = service.CreateCommand(SchedulerCommandId.SwitchToGroupByResource)
+                Dim menuItemCommandAdapter As SchedulerMenuItemCommandWinAdapter = New SchedulerMenuItemCommandWinAdapter(cmd)
+                Dim menuItem As DXMenuItem = CType(menuItemCommandAdapter.CreateMenuItem(DXMenuItemPriority.Normal), DXMenuItem)
+                menuItem.BeginGroup = True
+                e.Menu.Items.Add(menuItem)
+                ' Insert a new item into the Scheduler popup menu and handle its click event.
+                e.Menu.Items.Add(New SchedulerMenuItem("Click me!", AddressOf MyClickHandler))
+            End If
+        End Sub
 
-        ' Remove unnecessary items.
-        e.Menu.RemoveMenuItem(SchedulerMenuItemId.NewAllDayEvent)
-
-        ' Disable the "New Recurring Appointment" menu item.
-        e.Menu.DisableMenuItem(SchedulerMenuItemId.NewRecurringAppointment)
-
-        ' Find the "New Appointment" menu item and rename it.
-        Dim item As SchedulerMenuItem = e.Menu.GetMenuItemById(SchedulerMenuItemId.NewAppointment)
-        If item IsNot Nothing Then
-            item.Caption = "&New Meeting"
-        End If
-
-        ' Create a menu item for the Scheduler command.
-        Dim service As ISchedulerCommandFactoryService = DirectCast(schedulerControl1.GetService(GetType(ISchedulerCommandFactoryService)), ISchedulerCommandFactoryService)
-        Dim cmd As SchedulerCommand = service.CreateCommand(SchedulerCommandId.SwitchToGroupByResource)
-        Dim menuItemCommandAdapter As New SchedulerMenuItemCommandWinAdapter(cmd)
-        Dim menuItem As DXMenuItem = CType(menuItemCommandAdapter.CreateMenuItem(DXMenuItemPriority.Normal), DXMenuItem)
-        menuItem.BeginGroup = True
-        e.Menu.Items.Add(menuItem)
-
-        ' Insert a new item into the Scheduler popup menu and handle its click event.
-        e.Menu.Items.Add(New SchedulerMenuItem("Click me!", AddressOf MyClickHandler))
-    End If
-End Sub
-
-Public Sub MyClickHandler(ByVal sender As Object, ByVal e As EventArgs)
-    MessageBox.Show("My Menu Item Clicked!")
-End Sub
-#End Region ' #popupmenushowing
+        Public Sub MyClickHandler(ByVal sender As Object, ByVal e As EventArgs)
+            MessageBox.Show("My Menu Item Clicked!")
+        End Sub
+'#End Region  ' #popupmenushowing
     End Class
 End Namespace
